dc6ce7b31c0f15ab980b0904609c8e5240f94eab
Gumanit <ildarnagaev2004@gmail.com>
2026-04-07 20:14:11 +0500

    метод из лекции

--- Laba2/monte_karlo.go
+++ Laba2/monte_karlo.go
@@ -8,32 +8,24 @@ import (
 
 var num int = 1000000
 var x_min, x_max float64 = -1, 1
-var y_min, y_max = 0.0, 1.3
 
 func main() {
-
-	var up, down float64
-
+	summa := 0.0
+	sumSqLf := 0.0
 	for i := 0; i < num; i++ {
-		x := x_min + rand.Float64()*(x_max-x_min)
-		y := y_min + rand.Float64()*(y_max-y_min)
-
-		funcVal := (1 + math.Pow(x, 2)) / (1 + math.Pow(x, 4))
-		if y > funcVal {
-			up++
-		} else {
-			down++
-		}
+		x := x_min + (x_max-x_min)*rand.Float64()
+		fx := (1 + math.Pow(x, 2)) / (1 + math.Pow(x, 4))
+		summa += fx
+		sumSqLf += math.Pow((x_max-x_min)*fx, 2)
 	}
-
-	rectArea := (x_max - x_min) * (y_max - y_min)
-	w := down / (up + down)
-	I := rectArea * w
-	sqrtVal := math.Sqrt((w * (1.0 - w)) / float64(num))
-	delta_I := rectArea * 1.96 * sqrtVal
-	fmt.Printf("Общее количество точек = %v\n", num)
-	fmt.Printf("Доля точек ниже графика функции: %v\n", w)
-	fmt.Printf("I = %v\n", I)
-	fmt.Printf("Доверительный интервал: [%v, %v], delta I = %v\n", I-delta_I, I+delta_I, delta_I)
-	fmt.Printf("I точное = %v\n", 2.2214415)
+	w := (x_max - x_min) * (summa / float64(num))
+	disp := (sumSqLf - float64(num)*w*w) / (float64(num) - 1)
+	err := math.Sqrt(disp / float64(num))
+	halfWidth := 1.96 * err
+	integValue := 2.2214415
+	fmt.Printf("Значение оценки w = %.7f\n", w)
+	fmt.Printf("Истинное значение интеграла = %v\n", integValue)
+	fmt.Printf("Погрешность = %.7f\n", w-integValue)
+	fmt.Printf("Доверительный интервал: [%.7f, %.7f]\n", w-halfWidth, w+halfWidth)
+	fmt.Printf("Значение дисперсии S^2 = %.7f\n", disp)
 }
